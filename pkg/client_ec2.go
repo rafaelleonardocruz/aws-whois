b7422338501425e2f013182e96e064e701976a5f
Rafael Leonardo da Cruz <rafael.leonardo@creditas.com.br>
2020-12-19 12:31:03 -0300

    Add client to ec2

---
+++ pkg/client_ec2.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+type Instance struct {
+	InstanceID string
+	IpAddress  string
+}
+
+func getInstances() ([]Instance, error) {
+	var result []Instance
+
+	ec2client := ec2.New(session.New())
+	resp, err := ec2client.DescribeInstances(nil)
+
+	if err != nil {
+		return nil, errors.New("Error on ec2 client")
+	}
+
+	for i, _ := range resp.Reservations {
+		for _, instance := range resp.Reservations[i].Instances {
+			if *instance.PublicDnsName != "" {
+				result = append(result, Instance{*instance.InstanceId, *instance.PublicIpAddress})
+			}
+		}
+	}
+
+	return (result), nil
+}
