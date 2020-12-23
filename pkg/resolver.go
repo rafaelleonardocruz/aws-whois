63ecbd828cab69f2de80418f49395ce6c6f79c27
Rafael Leonardo da Cruz <rafael.leonardo@creditas.com.br>
2020-12-23 10:51:19 -0300

    Move region list to pkg level scope

--- pkg/resolver.go
+++ pkg/resolver.go
@@ -65,33 +65,9 @@ func isAwsAddress(a string) (bool, error) {
 }
 
 func getAwsRegion(hostname string) (region string, err error) {
-	regionList := []string{
-		"us-east-2",
-		"us-east-1",
-		"us-west-1",
-		"us-west-2",
-		"af-south-1",
-		"ap-east-1",
-		"ap-south-1",
-		"ap-northeast-3",
-		"ap-northeast-2",
-		"ap-southeast-1",
-		"ap-southeast-2",
-		"ap-northeast-1",
-		"ca-central-1",
-		"eu-central-1",
-		"eu-west-1",
-		"eu-west-2",
-		"eu-south-1",
-		"eu-west-3",
-		"eu-north-1",
-		"me-south-1",
-		"sa-east-1",
-	}
-
 	var found string
 
-	for _, r := range regionList {
+	for _, r := range RegionList {
 		if strings.Contains(hostname, r) {
 			found = r
 		}
